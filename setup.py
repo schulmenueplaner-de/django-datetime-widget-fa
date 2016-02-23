79015bdf9b1083eabc1b4ad7a466db39e9462fcf
Kamo Petrosyan <kamo@haikson.com>
2016-02-23 06:48:07 +0600

    up version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from setuptools import setup, find_packages
 
 setup(name = "django-datetime-widget-fa",
-    version = "0.9.7",
+    version = "0.10.0",
     description = "Django-datetime-widget is a simple and clean widget for DateField, Timefiled and DateTimeField  in Django framework. It is based on Bootstrap datetime picker, supports both Bootstrap 3 and Bootstrap 2, and supports Font-Awesome. Special thanks to Alfredo Saglimbeni [https://github.com/asaglimbeni] for django-datetime-widget",
     long_description=open('README.rst').read(),
     author = "Kamo Petrosyan",
